88f42a4d09d6151dafde88bf9530c8540e5f60cd
Adam Russell <adamlr6+pub@gmail.com>
2012-03-05 19:43:59 -0600

    Reload libexiv2python in libexiv2python.py.

--- src/libexiv2python.py
+++ src/libexiv2python.py
@@ -9,4 +9,5 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
                                                 '..', 'build')))
+reload(sys.modules['libexiv2python'])
 from libexiv2python import *
